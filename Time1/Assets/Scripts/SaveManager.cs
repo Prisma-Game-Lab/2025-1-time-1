1641405e34fe6a5170ee012ca798db80d91ca828
jomigmonte <jomigmonte@gmail.com>
2025-06-08 23:11:39 -0300

    diferentes arquivos para save

--- Time1/Assets/Scripts/SaveManager.cs
+++ Time1/Assets/Scripts/SaveManager.cs
@@ -6,40 +6,42 @@ using System;
 
 public class SaveManager : MonoBehaviour
 {
-    [SerializeField] GameObject dialogueScene;
-    private string filePath;
+    [SerializeField] private GameObject dialogueScene;
+    private string[] filePath = new string[3];
 
     void Start()
     {
-        filePath = Application.streamingAssetsPath + "/save.txt";
+        filePath[0] = Application.streamingAssetsPath + "/save.txt";
+        filePath[1] = Application.streamingAssetsPath + "/save1.txt";
+        filePath[2] = Application.streamingAssetsPath + "/save2.txt";
     }
 
-    public string[] readSave()
+    public string[] readSave(int save)
     {
-        string[] saveText = File.ReadAllLines(filePath);
+        string[] saveText = File.ReadAllLines(filePath[save]);
         return saveText;
     }
 
-    public void writeSave(int index, string line)
+    public void writeSave(int index, string line, int save)
     {
-        string[] saveText = readSave();
+        string[] saveText = readSave(save);
         if (saveText.Length > index)
         {
             saveText[index] = line;
         }
-        File.WriteAllLines(filePath, saveText);
+        File.WriteAllLines(filePath[save], saveText);
     }
 
-    public void save()
+    public void save(int save)
     {
         int index = dialogueScene.GetComponent<DialogueHandler>().currIndex;
         string indexLine = "scene: " + index + "\n";
-        writeSave(0, indexLine);
+        writeSave(0, indexLine, save);
     }
 
-    public void load()
+    public void load(int save)
     {
-        string[] saveText = readSave();
+        string[] saveText = readSave(save);
         int index = 0;
         Int32.TryParse(saveText[0].Split(':')[1], out index);
         dialogueScene.GetComponent<DialogueHandler>().dialogueIndex = index;
